97e537c21b313ee162d998c266d77b04e0cc4f4d
asvdev <57688070+asvdvl@users.noreply.github.com>
2021-05-21 22:35:00 +0300

    Added database support *Rewritten all operations with lists to supported by the database Added log levels Added exception handling to OnClientReady Added support for empty descriptions for fields in embeds Added a choice of whether to mention the user in the table of answers

--- DiscordRoleManager/Program.cs
+++ DiscordRoleManager/Program.cs
@@ -3,33 +3,20 @@ using Discord.Rest;
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using static ColorLogger.ColorLogger;
 
 namespace DiscordRoleManager
 {
     class Program
     {
-        //Maybe in the future, this will be stored normally (e.g. in db), but right now it's constants, sorry.
-        const ulong WorkGuildId = 694935732011270205;
-        const ulong WorkTextChannelId = 824940342457401354;
-        ulong WorkMessageId = 829428255262113883;
-
-        readonly List<string> stringsForAnswer = new List<string> {
-            "атыбись!", "че надо, я занят!!1!", ", извините, но я сегодня слишком занят для бесполезного общения", "",
-            "пожалуйся, не отвлекайте меня", "вы что-то сказали, я не расслышал?", "ты опять выходишь на связь, мудило?",
-            "418", "бот сломан, по всем вопросам писать @Deleteduser"};
-        readonly List<EmoteAndRoleRelation> emoteAndRoleRelationsList = new List<EmoteAndRoleRelation>
-        {
-            new EmoteAndRoleRelation { EmoteName = "kot666", RoleId = 829279908743872553, EmbedDescription = "Дает доступ к эксперементальному разделу." },     //rofl member
-            new EmoteAndRoleRelation { EmoteName = "Osu", RoleId = 829808235082285106, EmbedDescription = "Дает доступ к осу чату." },        //osu-reader
-            new EmoteAndRoleRelation { EmoteName = "Factorio", RoleId = 829808241516085249, EmbedDescription = "Дает доступ к факторио чату." },   //factorio-reader
-            new EmoteAndRoleRelation { EmoteName = "kubi", RoleId = 829808245873967114, EmbedDescription = "Дает доступ к майнкрафт чату." },       //minecraft-reader
-        };
-
+        readonly Settings st = Settings.GetInstance();
         //runtime vars
-        readonly Random rnd = new Random();
+        readonly Random rnd = new();
         SocketTextChannel workTextChannel;
         RestUserMessage workTextMessage;
 
@@ -37,43 +24,90 @@ namespace DiscordRoleManager
 
         public static void Main()
         => new Program().MainAsync().GetAwaiter().GetResult();
-
+        
         public async Task MainAsync()
         {
             _client = new DiscordSocketClient();
             _client.Ready += OnClientReady;
-
-            Console.WriteLine("Start bot...");
-            var token = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "token.key");
-            Console.WriteLine("Token was readed");
-            Console.WriteLine("Login...");
-
+            
+            Log("Start bot...");
+            var token = "";
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "token.key"))
+            {
+                token = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "token.key");
+            }
+            else
+            {
+                Log(LogLevel.Critical, "File with token not found. Exiting...");
+                Environment.Exit(1);
+            }
+            Log("Token was readed");
+            Log("Login...");
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
-            Console.WriteLine("Login succesful.");
+            Log("Login succesful.");
 #if DEBUG
-            Console.WriteLine("Debug build detected");
+            Log(LogLevel.Debug, "Debug build detected");
             await _client.SetStatusAsync(UserStatus.DoNotDisturb);
             await _client.SetGameAsync("Debug mode. start time: " + DateTime.Now, null, ActivityType.Playing);
+
+            //To debug via ssh
+            Log(LogLevel.Debug, "Waiting for debugger to attach");
+            for (int i = 0; i < 50 && !Debugger.IsAttached; i++)
+            {
+                if (!Debugger.IsAttached)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+            if (Debugger.IsAttached)
+            {
+                Log(LogLevel.Debug, "Debugger attached");
+            }
+            else
+            {
+                Log(LogLevel.Debug, "Debugger connection timeout");
+            }
 #endif
-            Console.WriteLine("Start complete.");
+            Log("Start complete.");
             await Task.Delay(-1);
         }
 
         private async Task OnClientReady()
         {
-            Console.WriteLine("OnClientReady");
-
+            Log(LogLevel.Debug, "OnClientReady");
+            //generate fields for embed
             var fields = new List<EmbedFieldBuilder>();
-            foreach (var relation in emoteAndRoleRelationsList)
+            try
             {
-                var role = _client.Guilds.SelectMany(guild => guild.Roles).First(roles => roles.Id == relation.RoleId);
-                var field = new EmbedFieldBuilder
+                foreach (var relation in st.EmoteAndRoleRelationsList.Query().ToList())
                 {
-                    Name = relation.isEmoji ? relation.EmoteName : role.Guild.Emotes.First(emote => emote.Name == relation.EmoteName).ToString(),
-                    Value = relation.EmbedDescription
-                };
-                fields.Add(field);
+                    var role = _client.Guilds.SelectMany(guild => guild.Roles).First(roles => roles.Id == relation.RoleId);
+
+                    var Name = (relation.IsEmoji ? relation.EmoteName : role.Guild.Emotes.First(emote => emote.Name == relation.EmoteName).ToString()) + $" ({role.Name})";
+                    var Value = relation.EmbedDescription;
+
+                    var field = new EmbedFieldBuilder();
+                    if (Name != null && Name.Length > 0)
+                    {
+                        field.WithName(Name);
+                    }
+                    if (Value != null && Value.Length > 0)
+                    {
+                        field.WithValue(Value);
+                    }
+                    else
+                    {
+                        field.WithValue("\u200b");
+                    }
+
+                    fields.Add(field);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(LogLevel.Error, ex.Message + "\n" + ex.StackTrace);
+                return;
             }
 
             var em = new EmbedBuilder
@@ -84,26 +118,27 @@ namespace DiscordRoleManager
                 Footer = new EmbedFooterBuilder { Text = "Discord role manager" }
             };
 
-            workTextChannel = _client.GetGuild(WorkGuildId).GetTextChannel(WorkTextChannelId);
+            workTextChannel = _client.GetGuild(st.WorkGuildId).GetTextChannel(st.WorkTextChannelId);
 
-            workTextMessage = (RestUserMessage) await workTextChannel.GetMessageAsync(WorkMessageId, new RequestOptions { Timeout = 5000});
+            workTextMessage = (RestUserMessage) await workTextChannel.GetMessageAsync(st.WorkMessageId, new RequestOptions { Timeout = 5000});
             if (workTextMessage == null)
             {
                 workTextMessage = await workTextChannel.SendMessageAsync(null, false, em.Build());
-                WorkMessageId = workTextMessage.Id;
-                Console.WriteLine($"rewrite workMessageId, new value is {WorkMessageId}");
+                st.WorkMessageId = workTextMessage.Id;
+                Log(LogLevel.Warning, $"rewrite workMessageId, new value is {st.WorkMessageId}");
             }
             else
             {
-                await workTextMessage.ModifyAsync(x => x.Embed = em.Build());          
-                Console.WriteLine("Work message is found");
+                await workTextMessage.ModifyAsync(x => x.Embed = em.Build());
+                Log("Work message is found");
             }
 
-            List<IEmote> reactionsToAdd = new List<IEmote>();
+            //Making a list to add to the message
+            List<IEmote> reactionsToAdd = new();
 
-            foreach (var relation in emoteAndRoleRelationsList)
+            foreach (var relation in st.EmoteAndRoleRelationsList.Query().ToList())
             {
-                if (relation.isEmoji)
+                if (relation.IsEmoji)
                 {
                     reactionsToAdd.Add(new Emoji(relation.EmoteName));
                 }
@@ -122,23 +157,34 @@ namespace DiscordRoleManager
 
         private async Task OnMessageReceived(SocketMessage messageParam)
         {
-            if (!(messageParam is SocketUserMessage message) || message.Author.Id == _client.CurrentUser.Id) return;
+            if (messageParam is not SocketUserMessage message || message.Author.Id == _client.CurrentUser.Id) return;
 
             if (message.Content.Contains(_client.CurrentUser.Id.ToString()))
             {
-                await message.Channel.SendMessageAsync($"<@{message.Author.Id}> " + stringsForAnswer[rnd.Next(stringsForAnswer.Count)]);
+                var answer = 
+                    st.StringsForAnswer.Query().Limit(1).Offset(
+                        rnd.Next(st.StringsForAnswer.Count())
+                        ).SingleOrDefault();
+
+                var sendingstring = answer.NeedPing? $"<@{message.Author.Id}> " : "";
+                sendingstring += answer.Answer;
+                await message.Channel.SendMessageAsync(sendingstring);
             }
             else if (message.Channel.GetType() == typeof(SocketDMChannel))
             {
-                await message.Channel.SendMessageAsync(stringsForAnswer[rnd.Next(stringsForAnswer.Count)]);
+                await message.Channel.SendMessageAsync(
+                    st.StringsForAnswer.Query().Limit(1).Offset(
+                        rnd.Next(st.StringsForAnswer.Count())
+                        ).SingleOrDefault().Answer
+                    );
             }
-            Console.WriteLine($"{message.Author.Username} \"{message.Content}\" message");
+            Log(LogLevel.Debug, $"{message.Author.Username} \"{message.Content}\" message");
         }
 
         private List<IRole> GetRolesFromRelation(SocketTextChannel currentWorkClannel, SocketReaction reaction)
         {
             var roles = new List<IRole>();
-            emoteAndRoleRelationsList.Where(x => x.EmoteName == reaction.Emote.Name)
+            st.EmoteAndRoleRelationsList.Query().Where(x => x.EmoteName == reaction.Emote.Name)
                 .Select(x => x.RoleId).ToList().ForEach(roleId =>
                     roles.Add(currentWorkClannel.Guild.GetRole(roleId)));
             return roles;
@@ -146,12 +192,12 @@ namespace DiscordRoleManager
 
         private async Task OnReactionRemoved(Cacheable<IUserMessage, ulong> _, ISocketMessageChannel textChannel, SocketReaction reaction)
         {
-            Console.WriteLine($"OnReactionRemoved, messageid: {reaction.MessageId} Name: " +
+            Log(LogLevel.Debug, $"OnReactionRemoved, messageid: {reaction.MessageId} Name: " +
                 $"{reaction.Emote.Name} {(reaction.Emote.Name.Length == 1 ? Char.ConvertToUtf32(reaction.Emote.Name, 0) : ' ')} " +
                 $"by: {reaction.UserId}");
             if (reaction.MessageId.Equals(workTextMessage.Id) && reaction.UserId != _client.CurrentUser.Id)
             {
-                Console.WriteLine($"Reaction removed for work message");
+                Log($"Reaction removed for work message");
                 var currentWorkClannel = textChannel as SocketTextChannel;
                 var currentWorkUser = currentWorkClannel.Guild.GetUser(reaction.UserId);
 
@@ -161,19 +207,19 @@ namespace DiscordRoleManager
                 }
                 else
                 {
-                    Console.WriteLine("ERROR: currentWorkUser is null!");
+                    Log(LogLevel.Warning, "currentWorkUser is null!");
                 }
             }
         }
 
         private async Task OnReactionAdded(Cacheable<IUserMessage, ulong> _, ISocketMessageChannel textChannel, SocketReaction reaction)
         {
-            Console.WriteLine($"OnReactionAdded, messageid: {reaction.MessageId} Name: " +
+            Log(LogLevel.Debug, $"OnReactionAdded, messageid: {reaction.MessageId} Name: " +
                 $"{reaction.Emote.Name} {(reaction.Emote.Name.Length == 1 ? Char.ConvertToUtf32(reaction.Emote.Name, 0) : ' ')} " +
                 $"by: {reaction.UserId}");
             if (reaction.MessageId.Equals(workTextMessage.Id) && reaction.UserId != _client.CurrentUser.Id)
             {
-                Console.WriteLine($"Reaction added for work message");
+                Log($"Reaction added for work message");
                 var currentWorkClannel = textChannel as SocketTextChannel;
                 var currentWorkUser = currentWorkClannel.Guild.GetUser(reaction.UserId);
 
@@ -183,17 +229,11 @@ namespace DiscordRoleManager
                 }
                 else
                 {
-                    Console.WriteLine("ERROR: currentWorkUser is null!");
+                    Log(LogLevel.Warning, "currentWorkUser is null!");
                 }
             }
         }
 
-        struct EmoteAndRoleRelation
-        {
-            public string EmoteName;
-            public bool isEmoji;
-            public string EmbedDescription;
-            public ulong RoleId;
-        }
+        
     }
 }
---
+++ DiscordRoleManager/Settings.cs
@@ -0,0 +1,51 @@
+﻿using LiteDB;
+using System;
+using static ColorLogger.ColorLogger;
+
+namespace DiscordRoleManager
+{
+    public class Settings
+    {
+        //on linux systems, if left without "AppDomain.CurrentDomain.BaseDirectory", 
+        //the database was created in the user's home directory, which is sometimes not allowed, 
+        //so here is a hard link to the directory with the program
+        readonly LiteDatabase db = new(AppDomain.CurrentDomain.BaseDirectory + "Settings.db");
+        public Settings()
+        {
+            Log("Try restore data from db");
+            EmoteAndRoleRelationsList = db.GetCollection<EmoteAndRoleRelation>("EmoteRelation") as LiteCollection<EmoteAndRoleRelation>;
+            StringsForAnswer = db.GetCollection<AnswersStrings>("StringsForAnswer") as LiteCollection<AnswersStrings>;
+        }
+
+        public ulong WorkGuildId = 694935732011270205;
+        public ulong WorkTextChannelId = 824940342457401354;
+        public ulong WorkMessageId = 830157913405259807;
+
+        public LiteCollection<AnswersStrings> StringsForAnswer { get; set; }
+
+        public LiteCollection<EmoteAndRoleRelation> EmoteAndRoleRelationsList { get; set; }
+
+        private static Settings instance;
+        public static Settings GetInstance()
+        {
+            if (instance == null)
+                instance = new Settings();
+            return instance;
+        }
+        
+        public struct EmoteAndRoleRelation
+        {
+            public string EmoteName { get; set; }
+            public bool IsEmoji { get; set; }
+            public string EmbedDescription { get; set; }
+            [BsonId]
+            public ulong RoleId { get; set; }
+        }
+
+        public class AnswersStrings
+        {
+            public string Answer { get; set; }
+            public bool NeedPing { get; set; } = false;
+        }
+    }
+}
